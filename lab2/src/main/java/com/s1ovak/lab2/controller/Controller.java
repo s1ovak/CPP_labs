f2a93def6a405bec31291cc01143a0041a9bd14f
slovak <slava_muhin@tut.by>
2019-03-10 22:29:32 +0300

    Testing

--- lab2/src/main/java/com/s1ovak/lab2/controller/Controller.java
+++ lab2/src/main/java/com/s1ovak/lab2/controller/Controller.java
@@ -1,9 +1,11 @@
 package com.s1ovak.lab2.controller;
 
-import com.s1ovak.lab2.entity.NumberOfSymbols;
+import com.s1ovak.lab2.entity.Entity;
 import com.s1ovak.lab2.service.Service;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,16 +13,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/programm")
 public class Controller {
 
+    private Service service;
+
     @Autowired
-    Service service;
+    public Controller(Service service) {
+        this.service = service;
+    }
 
-
-    @RequestMapping
-    public NumberOfSymbols getData(
-            @RequestParam(name = "string") String string,
-            @RequestParam(name = "symbol") Character symbol
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public ResponseEntity<Entity> getEntity(
+            @RequestParam(name = "string", required = false) String string,
+            @RequestParam(name = "symbol", required = false) String symbol
     ) {
 
-        return service.calculateSymbol(string, symbol);
+        Entity entity = service.getEntity(string, symbol);
+        if(entity.getErrorMessage()!=null)
+            return ResponseEntity.status(400).body(entity);
+        return ResponseEntity.ok(entity);
     }
 }
--- lab2/src/main/java/com/s1ovak/lab2/service/Service.java
+++ lab2/src/main/java/com/s1ovak/lab2/service/Service.java
@@ -1,6 +1,7 @@
 package com.s1ovak.lab2.service;
 
-import com.s1ovak.lab2.entity.NumberOfSymbols;
+import com.s1ovak.lab2.controller.Controller;
+import com.s1ovak.lab2.entity.Entity;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Component;
 
@@ -8,18 +9,46 @@ import org.springframework.stereotype.Component;
 @Component
 public class Service {
 
-    private static final Logger logger = Logger.getLogger(Service.class.getName());
+    private static final Logger logger = Logger.getLogger(Controller.class);
 
-    public NumberOfSymbols calculateSymbol(String string, char symbol) {
-        NumberOfSymbols count = new NumberOfSymbols(0);
-        logger.info("This is log info");
+    public Entity getEntity(String string, String symbol) {
+
+        if (logger.isDebugEnabled()) {
+            logger.debug("getEntity method is called!");
+        }
+        if (string == null) {
+            return isError("Invalid params! Need parameter string");
+        }
+        if (symbol == null) {
+            return isError("Invalid params! Need parameter symbol");
+        }
+
+
+        if (symbol.length() != 1) {
+            return isError("Invalid params! Param symbol must be char");
+        }
+
+        logger.debug("getEntity method is successfully completed");
+        return calculateSymbol(string, symbol);
+    }
+
+
+    public Entity calculateSymbol(String string, String symbol) {
+        Entity count = new Entity(0);
+        char checkedSymbol = symbol.charAt(0);
         for (int i = 0; i < string.length(); i++) {
-            if (string.charAt(i) == symbol) {
-                count.setNumber(count.getNumber()+1);
+            if (string.charAt(i) == checkedSymbol) {
+                count.setNumber(count.getNumber() + 1);
             }
         }
 
         return count;
+    }
+
+
+    private Entity isError(String msg) {
+        logger.error(msg);
+        return new Entity(msg);
     }
 
 }
